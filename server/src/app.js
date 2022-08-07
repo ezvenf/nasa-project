e26be5e546f4746196508411ce57050d943b2084
ezvenfernandes <ezvenf@gmail.com>
2022-08-07 09:50:22 +0530

    Setup Production npm

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -12,6 +13,11 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 app.use(planetsRouter);
 
 module.exports = app;
